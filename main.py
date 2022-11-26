356a1ecd6d64b87c5b9953f4f074c8ed25278659
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-11-26 12:20:52 +0900

    minor fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from simulator import evaluate
 
 if __name__ == '__main__':
 
-    model_id = logger.generate_id('deeplob-lighten')
+    model_id = logger.generate_id('deeplob')
     train.train(
         model_id=model_id, dataset_type = 'fi2010', normalization= 'Zscore', lighten= False,
         T= 100, k= 4, stock= [0, 1, 2, 3, 4], train_test_ratio = 0.7)
