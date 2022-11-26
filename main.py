d8b791238e3422e475cca6279ba2a1b018008575
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-11-26 10:52:59 +0900

    minor fix

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from optimizers import train
 from simulator import evaluate
 
 if __name__ == '__main__':
-    model_id = logger.generate_id('deeplob-lighten')
-    train.train(
-        model_id=model_id, dataset_type = 'krx', normalization= 'DecPre', lighten= True,
-        T= 100, k= 100, stock= ['KS200', 'KQ150'], train_test_ratio = 0.7)
+
+    # model_id = logger.generate_id('deeplob-lighten')
+    # train.train(
+    #     model_id=model_id, dataset_type = 'krx', normalization= 'DecPre', lighten= True,
+    #     T= 100, k= 100, stock= ['KS200', 'KQ150'], train_test_ratio = 0.7)
+    # evaluate.test(model_id=model_id)
+    model_id = 'deeplob-lighten_2022-11-26_01:23:27'
     evaluate.test(model_id=model_id)
