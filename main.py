d290401dec6d54ec3853ffe43a2a4985c7f06a2f
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-11-26 11:39:20 +0900

    minor fix

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 
     model_id = logger.generate_id('deeplob-lighten')
     train.train(
-        model_id=model_id, dataset_type = 'fi2010', normalization= 'Zscore', lighten= True,
+        model_id=model_id, dataset_type = 'fi2010', normalization= 'Zscore', lighten= False,
         T= 100, k= 4, stock= [0, 1, 2, 3, 4], train_test_ratio = 0.7)
     evaluate.test(model_id=model_id)
