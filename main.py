c86d832485c8e1a4b3464a910074fcba439a1cd0
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-12-03 06:28:35 +0900

    minor fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     model_id = logger.generate_id('deeplob-lighten')
     train.train(
         model_id=model_id, dataset_type = 'krx', normalization= 'Zscore', lighten= True,
-        T= 100, k= 10, stock= ["KQ150"], train_test_ratio = 0.6)
+        T= 100, k= 50, stock= ["KQ150"], train_test_ratio = 0.6)
     evaluate.test(model_id = model_id)
     #model_id = 'deeplob-lighten_2022-12-02_21:01:39'
     backtest(model_id = model_id)
