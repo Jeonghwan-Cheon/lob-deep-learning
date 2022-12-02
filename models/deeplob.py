d7719f3670c48775784f16bbc139121b4aaff377
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-12-02 14:31:52 +0900

    minor fix

--- models/deeplob.py
+++ models/deeplob.py
@@ -52,35 +52,35 @@ class Deeplob(nn.Module):
 
         # inception modules
         self.inp1 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 1), padding='same'),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
         )
         self.inp2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5, 1), padding='same'),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(5, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
         )
         self.inp3 = nn.Sequential(
             nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0)),
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(64),
+            nn.BatchNorm2d(32),
         )
 
         # lstm layers
-        self.lstm = nn.LSTM(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
-        self.fc1 = nn.Linear(64, 3)
+        self.lstm = nn.LSTM(input_size=96, hidden_size=32, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(32, 3)
 
     def forward(self, x):
-        h0 = torch.zeros(1, x.size(0), 64).to(self.device)
-        c0 = torch.zeros(1, x.size(0), 64).to(self.device)
+        h0 = torch.zeros(1, x.size(0), 32).to(self.device)
+        c0 = torch.zeros(1, x.size(0), 32).to(self.device)
 
         x = self.conv1(x)
         x = self.conv2(x)
--- models/lobster.py
+++ models/lobster.py
@@ -1,58 +1,35 @@
-import math as m
 import torch
-from torch import nn, Tensor
-from torch.nn import TransformerEncoder, TransformerEncoderLayer
+import torch.nn as nn
 
-class PositionalEncoding(nn.Module):
-
-    def __init__(self, d_model: int, dropout: float = 0.1, max_len: int = 5000):
-        super().__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        position = torch.arange(max_len).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2) * (-m.log(10000.0) / d_model))
-        pe = torch.zeros(max_len, 1, d_model)
-        pe[:, 0, 0::2] = torch.sin(position * div_term)
-        pe[:, 0, 1::2] = torch.cos(position * div_term)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x: Tensor) -> Tensor:
-        """
-        Args:
-            x: Tensor, shape [seq_len, batch_size, embedding_dim]
-        """
-        x = x + self.pe[:x.size(0)]
-        return self.dropout(x)
-
-class Lobster(nn.Module):
+class Deeplob(nn.Module):
     def __init__(self, lighten):
         super().__init__()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        self.name = 'lobster'
+        self.name = 'deeplob'
         if lighten:
             self.name += '-lighten'
 
         # convolution blocks
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(1, 2), stride=(1, 2)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
         )
         self.conv2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 2), stride=(1, 2)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
         )
 
@@ -63,46 +40,42 @@ class Lobster(nn.Module):
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, conv3_kernel_size)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(4, 1)),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(32),
         )
 
         # inception modules
         self.inp1 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(64),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 1), padding='same'),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(64),
         )
         self.inp2 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(64),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5, 1), padding='same'),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(64),
         )
         self.inp3 = nn.Sequential(
             nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0)),
             nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
-            nn.SiLU(),
+            nn.LeakyReLU(negative_slope=0.01),
             nn.BatchNorm2d(64),
         )
 
-        # transformer layers
+        # lstm layers
         self.lstm = nn.LSTM(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
-
-        self.pos_encoder = PositionalEncoding(d_model, dropout=0.1)
-
-        # mlp layers
         self.fc1 = nn.Linear(64, 3)
 
     def forward(self, x):
