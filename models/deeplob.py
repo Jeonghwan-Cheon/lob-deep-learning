963e75cee985a0ebff301cc46c0f832e36ffc8af
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-12-02 14:38:14 +0900

    minor fix

--- models/deeplob.py
+++ models/deeplob.py
@@ -52,34 +52,34 @@ class Deeplob(nn.Module):
 
         # inception modules
         self.inp1 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 1), padding='same'),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(64),
         )
         self.inp2 = nn.Sequential(
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(5, 1), padding='same'),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(5, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(64),
         )
         self.inp3 = nn.Sequential(
             nn.MaxPool2d((3, 1), stride=(1, 1), padding=(1, 0)),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(1, 1), padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(1, 1), padding='same'),
             nn.LeakyReLU(negative_slope=0.01),
-            nn.BatchNorm2d(32),
+            nn.BatchNorm2d(64),
         )
 
         # lstm layers
-        self.lstm = nn.GRU(input_size=96, hidden_size=32, num_layers=1, batch_first=True)
-        self.fc1 = nn.Linear(32, 3)
+        self.gru = nn.GRU(input_size=192, hidden_size=64, num_layers=1, batch_first=True)
+        self.fc1 = nn.Linear(64, 3)
 
     def forward(self, x):
-        h0 = torch.zeros(1, x.size(0), 32).to(self.device)
+        h0 = torch.zeros(1, x.size(0), 64).to(self.device)
 
         x = self.conv1(x)
         x = self.conv2(x)
@@ -94,7 +94,7 @@ class Deeplob(nn.Module):
         x = x.permute(0, 2, 1, 3)
         x = torch.reshape(x, (-1, x.shape[1], x.shape[2]))
 
-        x, _ = self.lstm(x, h0)
+        x, _ = self.gru(x, h0)
         x = x[:, -1, :]
         x = self.fc1(x)
         forecast_y = torch.softmax(x, dim=1)
