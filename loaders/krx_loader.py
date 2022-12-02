83005bd735a13da931b6809296b09294fe6ae93e
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-12-03 07:30:59 +0900

    minor fix

--- loaders/krx_loader.py
+++ loaders/krx_loader.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from loaders.krx_preprocess import get_normalized_data_list, get_processed_data_list
 
 
-def __split_x_y__(norm_data, proc_data, k, threshold=0, vis = True):
+def __split_x_y__(norm_data, proc_data, k, threshold=0.05/100, vis = False):
     """
     Extract lob data and annotated label from fi-2010 data
     Parameters
@@ -28,6 +28,8 @@ def __split_x_y__(norm_data, proc_data, k, threshold=0, vis = True):
 
         if l_i > threshold:
             y[i] = 1
+        elif l_i < -threshold:
+            y[i] = -1
         else:
             y[i] = 0
 
