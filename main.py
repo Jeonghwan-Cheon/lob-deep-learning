ae094dc67e4c775e9cd3644e0623e95c8d50090f
Jeonghwan-Cheon <jeonghwan518@icloud.com>
2022-12-03 20:29:44 +0900

    minor fix

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from simulator.training_vis import vis_training_process
 
 
 if __name__ == '__main__':
-    # model_id = logger.generate_id('lobster-lighten')
-    # train.train(
-    #     model_id=model_id, dataset_type = 'fi2010', normalization= 'Zscore', lighten= True,
-    #     T= 100, k= 4, stock= [0, 1, 2, 3, 4], train_test_ratio = 0.7, model_type='lobster')
-    model_id = 'lobster-lighten_2022-12-03_10:34:05'
-    evaluate.test(model_id = model_id, model_type='lobster')
+    model_id = logger.generate_id('deeplob_final')
+    train.train(
+        model_id=model_id, dataset_type = 'fi2010', normalization= 'Zscore', lighten= False,
+        T= 100, k= 4, stock= [0, 1, 2, 3, 4], train_test_ratio = 0.7, model_type='deeplob')
+    #model_id = 'lobster-lighten_2022-12-03_10:34:05'
+    evaluate.test(model_id = model_id, model_type='deeplob')
 
     # model_id = 'deeplob-lighten_selected_1'
     # vis_training_process(model_id = model_id)
